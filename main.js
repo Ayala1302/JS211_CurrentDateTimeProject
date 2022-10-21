19752d68aa0e902d01c46c1926d937ea14ef84d2
Ayala1302 <marcoayala48@yahoo.com>
2022-10-21 13:21:51 -0500

    almost finished

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTIONS!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
-
+let firstNum = null
+let secondNum = null
+let operation = null
 
 // ***************************
 //          PART ONE
@@ -12,14 +14,22 @@ const displayDate = () => {
 
   document.getElementById("display-element").innerHTML = currentDate;
 }
- 
+
 
 // Write a JavaScript program to convert a number to a string.
-
+const numberToString = num => String(num);
+let num = 15;
+let text = num.toString();
 
 
 // Write a JavaScript program to convert a string to the number.
+const saveFirstNumber = (num) => {
+  firstNum = parseInt(num)  
+}
 
+const saveSecondNumber = (num) => {
+  secondNum = parseInt(num)
+}
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -29,22 +39,42 @@ const displayDate = () => {
   // * Number
   // * NaN
   // * String
-  
+  const typeOf = data => Array.isArray(data) ? 'array' : typeof data;
 
-  
+
 // Write a JavaScript program that adds 2 numbers together.
+  const add = (numA, numB) => {
+  const sum = numA + numB
+  return sum
+}
 
+const changeOperation = (chosenOperation) => {
+  operation = chosenOperation
+  console.log(operation)
+}
 
+const putResultInElement = (operationResults) => {
+  document.getElementById("result").innerHTML = "Results: " + operationResults
+}
+
+const equals = () => {
+  switch (operation) {
+    case "addition":  putResultInElement(add(firstNum, secondNum)) 
+    break;
+  }
+}
 
 // Write a JavaScript program that runs only when 2 things are true.
-
+const bothTrue = (a, b) => a && b;
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
-
+const oneTrue = (a, b) => a || b;
 
 
 // Write a JavaScript program that runs when both things are not true.  
+const bothNotTrue = (a, b) => !a && !b;
+
 
 // ***************************
 //         PART TWO
@@ -58,6 +88,14 @@ const displayDate = () => {
 // 6. go to `index.html` 
 // 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
 
+// const addForm = document.getElementById('add');
+// addForm.addEventListener("submit", e => {
+//   e.preventDefault()
+//   const n1 = document.getElementById("number1").value;
+//   const n2 = document.getElementById("number2").value;
+//   const sum = add(stringToNumber(n1), stringToNumber(n2));
+//   document.getElementById("sum").innerHTML = `The sum of ${n1} + ${n2} is ${sum}`;
+// });
 
 
 
